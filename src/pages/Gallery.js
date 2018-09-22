0225c6f765f2ac563a48acdc17ddc19a3bb4b75b
Bruno Alla <alla.brunoo@gmail.com>
2018-09-22 13:21:42 +0100

    Make the gallery page work with Lightbox

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,68 +1,118 @@
 import { graphql, Link } from 'gatsby'
-import React from 'react'
+import React, { Component } from 'react'
 import Meta from 'components/Meta'
 import Layout from 'components/Layout'
 import get from 'lodash/get'
 import './gallery_style.scss'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
 
-const renderSharpImage = sharpImageNode => {
-  /* Example:
-  {
-      "node": {
-        "childImageSharp": {
-          "original": {
-            "src": "/static/IMG_2462_2-c8b21a05ebd64daed5c0176e0d47d45d.jpg",
-            "width": 1024
-          },
-          "resize": {
-            "src": "/static/IMG_2462_2-c8b21a05ebd64daed5c0176e0d47d45d-487e6.jpg",
-            "width": 400
-          }
-        }
-      }
+class Gallery extends Component {
+  constructor(props) {
+    super(props)
+
+    const images = props.data.allFile.edges.map(
+      edge => edge.node.childImageSharp.original.src
+    )
+    const thumbs = props.data.allFile.edges.map(
+      edge => edge.node.childImageSharp.resize.src
+    )
+
+    this.state = {
+      index: 0,
+      isOpen: false,
+      images: images,
+      thumbs: thumbs,
     }
-  */
-  const { original, resize } = sharpImageNode.node.childImageSharp
-  return (
-    <div className="col-md-3 col-sm-6" key={sharpImageNode.node.id}>
-      <div
-        className="m-1"
-        style={{
-          width: '100%',
 
-          paddingTop: '100%',
-        }}
-      >
-        <Link to={original.src}>
-          <span
-            className="img-square-bg-image"
-            style={{ backgroundImage: `url(${resize.src})` }}
-          />
-        </Link>
-      </div>
-    </div>
-  )
-}
+    this.renderLightBox = this.renderLightBox.bind(this)
+    this.openLightBox = this.openLightBox.bind(this)
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.movePrev = this.movePrev.bind(this)
+    this.moveNext = this.moveNext.bind(this)
+  }
 
-const Gallery = ({ data, location }) => {
-  const images = get(data, 'allFile').edges
-  console.log(images)
-  return (
-    <Layout location={location}>
-      <Meta site={get(data, 'site.meta')} />
-      <div className="container-fluid">
-        <div className="my-4">
-          <div className="row">
-            <div className="col-md-12">
-              <h2>Gallery de photos</h2>
-              <p>Voici quelques photos des lieux</p>
-              <div className="row">{images.map(renderSharpImage)}</div>
+  openLightBox(index) {
+    this.setState({
+      index: index,
+      isOpen: true,
+    })
+  }
+
+  renderLightBox() {
+    const { images, thumbs } = this.state
+    return (
+      <Lightbox
+        mainSrc={images[this.state.index]}
+        nextSrc={images[(this.state.index + 1) % images.length]}
+        prevSrc={images[(this.state.index + images.length - 1) % images.length]}
+        mainSrcThumbnail={thumbs[this.state.index]}
+        nextSrcThumbnail={thumbs[(this.state.index + 1) % images.length]}
+        prevSrcThumbnail={
+          thumbs[(this.state.index + images.length - 1) % images.length]
+        }
+        onCloseRequest={this.closeLightbox}
+        onMovePrevRequest={this.movePrev}
+        onMoveNextRequest={this.moveNext}
+      />
+    )
+  }
+
+  closeLightbox() {
+    this.setState({ isOpen: false })
+  }
+  movePrev() {
+    this.setState({
+      index: (this.state.index - 1) % this.state.images.length,
+    })
+  }
+  moveNext() {
+    this.setState({
+      index: (this.state.index + 1) % this.state.images.length,
+    })
+  }
+
+  render() {
+    let { data, location } = this.props
+
+    return (
+      <Layout location={location}>
+        <Meta site={get(data, 'site.meta')} />
+        <div className="container-fluid">
+          <div className="my-4">
+            <div className="row">
+              <div className="col-md-12">
+                <h1>Gallerie de photos</h1>
+                <p>Voici quelques photos des lieux</p>
+                <div className="row">
+                  {this.state.thumbs.map((thumbnail, index) => {
+                    return (
+                      <div className="col-md-3 col-sm-6" key={index}>
+                        <div
+                          className="m-1"
+                          style={{
+                            width: '100%',
+                            paddingTop: '100%',
+                          }}
+                          onClick={() => this.openLightBox(index)}
+                        >
+                          <span
+                            className="img-square-bg-image"
+                            style={{ backgroundImage: `url(${thumbnail})` }}
+                          />
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+                {this.state.isOpen && this.renderLightBox()}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 export default Gallery
