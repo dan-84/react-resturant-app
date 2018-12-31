7dc4b7efcc37842196755c170efa2259a84998b0
Bruno Alla <alla.brunoo@gmail.com>
2018-12-31 16:59:40 +0000

    Remove usage of a several dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,27 +25,7 @@ module.exports = {
     },
     {
       resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
-              wrapperStyle: 'margin-bottom: 1.0725rem;',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
+      options: {},
     },
     {
       resolve: `gatsby-plugin-manifest`,
