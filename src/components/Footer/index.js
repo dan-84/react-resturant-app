719d41345089093df52ad1c39471dd973cd11fae
Bruno Alla <alla.brunoo@gmail.com>
2020-01-04 16:30:31 +0000

    Replace Fortawesome by React icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,40 +1,45 @@
 import React from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import './style.scss'
-
-library.add(fab)
-
-const socialLinks = [
-  {
-    name: 'facebook',
-    url: 'https://www.facebook.com/ChezNicoleSauclieres/',
-  },
-  {
-    name: 'google',
-    url: 'https://goo.gl/maps/d8H73U3N73G2',
-  },
-  {
-    name: 'tripadvisor',
-    url:
-      'https://www.tripadvisor.fr/Restaurant_Review-g2435151-d5800149-Reviews-Chez_Nicole-Sauclieres_Aveyron_Occitanie.html',
-  },
-]
+import { FaFacebook, FaGoogle, FaTripadvisor } from 'react-icons/fa'
 
 const Footer = ({ author, title }) => (
   <div className="footer py-5 small">
     <div className="container">
-      <hr className="border-primary" />
+      <hr
+        className="border-primary"
+        style={{
+          borderWidth: '3px',
+          maxWidth: '50px',
+        }}
+      />
       <div className="text-center">
         <ul className="list-inline">
-          {socialLinks.map(link => (
-            <li className="list-inline-item" key={link.name}>
-              <a href={link.url} target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={['fab', link.name]} size="4x" />
-              </a>
-            </li>
-          ))}
+          <li className="list-inline-item">
+            <a
+              href="https://www.facebook.com/ChezNicoleSauclieres/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook size="4em" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a
+              href="https://goo.gl/maps/d8H73U3N73G2"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGoogle size="4em" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a
+              href="https://www.tripadvisor.fr/Restaurant_Review-g2435151-d5800149-Reviews-Chez_Nicole-Sauclieres_Aveyron_Occitanie.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTripadvisor size="4em" />
+            </a>
+          </li>
         </ul>
       </div>
       <div className="text-center">
