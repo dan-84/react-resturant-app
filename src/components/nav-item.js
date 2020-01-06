f360e502e49fc660fe5fc41ae2907deecc2bf66e
Bruno Alla <alla.brunoo@gmail.com>
2020-01-06 19:27:50 +0000

    Change NavItem to a functional component

--- src/components/nav-item.js
+++ src/components/nav-item.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-class NavItem extends React.Component {
-  render() {
-    const { location, url, children } = this.props
-
-    return (
-      <li
-        className={location.pathname === url ? 'nav-item active' : 'nav-item'}
-      >
-        <Link to={url} className="nav-link">
-          {children}
-        </Link>
-      </li>
-    )
-  }
+const NavItem = props => {
+  const { location, url, children } = props
+  return (
+    <li className={location.pathname === url ? 'nav-item active' : 'nav-item'}>
+      <Link to={url} className="nav-link">
+        {children}
+      </Link>
+    </li>
+  )
 }
 
 export default NavItem
