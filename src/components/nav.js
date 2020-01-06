626d7f105b06c80dfe9db7077062847b5ddd0441
Bruno Alla <alla.brunoo@gmail.com>
2020-01-06 19:24:09 +0000

    Change Nav component as a arrow functional component

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,40 +2,38 @@ import React from 'react'
 import { Link } from 'gatsby'
 import NaviItem from './nav-item'
 
-class Nav extends React.Component {
-  render() {
-    const { location, title } = this.props
-    return (
-      <React.Fragment>
-        <div className="row mx-1">
-          <Link to="/" className="navbar-brand">
-            <img src="/img/logo.png" alt={title} className="img-fluid" />
-          </Link>
-        </div>
-        <div className="m-b-1">
-          <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
-            <div className="navbar-nav-scroll">
-              <ul className="navbar-nav bd-navbar-nav flex-row">
-                <NaviItem url="/" location={location}>
-                  Accueil
-                </NaviItem>
-                <NaviItem url="/restaurant/" location={location}>
-                  Restaurant
-                </NaviItem>
-                <NaviItem url="/contact/" location={location}>
-                  Contact
-                </NaviItem>
-                <NaviItem url="/photos/" location={location}>
-                  Photos
-                </NaviItem>
-              </ul>
-            </div>
-            <div className="navbar-nav flex-row ml-md-auto d-none d-md-flex" />
-          </nav>
-        </div>
-      </React.Fragment>
-    )
-  }
+const Nav = props => {
+  const { location, title } = props
+  return (
+    <React.Fragment>
+      <div className="row mx-1">
+        <Link to="/" className="navbar-brand">
+          <img src="/img/logo.png" alt={title} className="img-fluid" />
+        </Link>
+      </div>
+      <div className="m-b-1">
+        <nav className="navbar navbar-expand navbar-dark flex-column flex-md-row bg-primary">
+          <div className="navbar-nav-scroll">
+            <ul className="navbar-nav bd-navbar-nav flex-row">
+              <NaviItem url="/" location={location}>
+                Accueil
+              </NaviItem>
+              <NaviItem url="/restaurant/" location={location}>
+                Restaurant
+              </NaviItem>
+              <NaviItem url="/contact/" location={location}>
+                Contact
+              </NaviItem>
+              <NaviItem url="/photos/" location={location}>
+                Photos
+              </NaviItem>
+            </ul>
+          </div>
+          <div className="navbar-nav flex-row ml-md-auto d-none d-md-flex" />
+        </nav>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default Nav
